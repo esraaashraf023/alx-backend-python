553406f214528bffa1f927c3ff0a6c6db7f83510
esraaashraf023 <esraaashraf780567@gmail.com>
2024-04-19 02:55:28 +0200

    add file5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type annotated task 5
+"""
+import typing
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """
+    return their sum
+    """
+    return sum(input_list)
