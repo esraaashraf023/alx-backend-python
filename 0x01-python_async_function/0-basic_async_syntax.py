ca5847a51207e6db96856334a40f1ec342a65243
esraaashraf023 <esraaashraf780567@gmail.com>
2024-04-22 21:13:20 +0200

    update

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,7 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    asynchronous coroutine that waits for random delay between 0 and max_delay
+     an asynchronous coroutine that takes in an integer argument..
     """
     Q_max_delay = random.uniform(0, max_delay)
     await asyncio.sleep(Q_max_delay)
