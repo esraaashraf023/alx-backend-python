bba995ad5181697ccdf500fc3bc8f477a533b86c
esraaashraf023 <esraaashraf780567@gmail.com>
2024-04-22 22:01:43 +0200

    update

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,5 +6,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """do not create an async function, use the regular function syntax to do this"""
+    """do not create an async function, use the regular..."""
     return asyncio.create_task(wait_random(max_delay))
