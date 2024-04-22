19b8341dfe180870baa12bc1e4fc32b672075ee1
esraaashraf023 <esraaashraf780567@gmail.com>
2024-04-22 22:08:01 +0200

    updata

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Take the code from wait_n and alter it into a new function task_wait_n.."""
+    """Take the code from wait_n"""
     random_delay_list: List[float] = []
 
     for i in range(n):
