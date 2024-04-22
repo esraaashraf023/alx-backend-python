9e873c5824a560d2a8e273c7b3e797d66617a390
esraaashraf023 <esraaashraf780567@gmail.com>
2024-04-22 21:43:15 +0200

    Task

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-"""takes 2..."""
-
-
+"""execute multiple coroutines at the same time with async"""
 import asyncio
 from typing import List
 
-task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ that takes in 2 int arguments (in this order):n and max_delay.
-    You will spawn wait_random n times with the specified max_delay."""
-    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
-    results = await asyncio.gather(*tasks)
-    return sorted(results)
+    """coroutine that call wait_random n times"""
+    random_delay_list: List[float] = []
+
+    for i in range(n):
+        random_delay_list.append(await wait_random(max_delay))
+
+    return sorted(random_delay_list)
