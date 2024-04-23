bf8892195dace103a8ca94e7653f08e930a283b3
esraaashraf023 <esraaashraf780567@gmail.com>
2024-04-24 00:53:56 +0200

    Task

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+task 0
+"""
+
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> typing.Generator[float, None, None]:
+    """
+    Write a coroutine called async_generator
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
