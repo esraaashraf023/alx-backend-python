9f7808aa650b928bcd43b0afbfeb1334130f45fd
esraaashraf023 <esraaashraf780567@gmail.com>
2024-04-24 01:02:18 +0200

    Task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Task 1
+"""
+
+import typing
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> typing.List[float]:
+    """
+    Import async_generator from the previous task
+    """
+    output = []
+    async for value in async_generator():
+        output.append(value)
+
+    return output
